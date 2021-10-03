6c6c50f526405d1a60fc01a19edecd9fce3d981b
ff1892 <hitrulein@gmail.com>
2021-10-03 19:09:19 +0300

    Добавляет компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const InitialSettings = {
+  CARD_COUNT: 5,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      cardCount = {InitialSettings.CARD_COUNT}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
